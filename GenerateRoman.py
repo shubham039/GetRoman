c1088f1328892630ee935512a6b6e550016b9897
shubham039 <32260408+shubham039@users.noreply.github.com>
2017-12-08 17:38:05 +0530

    Update GenerateRoman.py

--- GenerateRoman.py
+++ GenerateRoman.py
@@ -8,39 +8,31 @@ Created on Fri Dec  8 16:49:15 2017
 
 from collections import OrderedDict
 
+def GenerateRoman(num):
 
-class GenerateRoman:
-    def __init__(self):
-        ''' Constructor for this class. '''
-        # Create some member animals
-        self.members = ['Tiger', 'Elephant', 'Wild Cat']
- 
- 
-    def GenerateRoman(self,num):
+    roman = OrderedDict()
+    roman[1000] = "M"
+    roman[900] = "CM"
+    roman[500] = "D"
+    roman[400] = "CD"
+    roman[100] = "C"
+    roman[90] = "XC"
+    roman[50] = "L"
+    roman[40] = "XL"
+    roman[10] = "X"
+    roman[9] = "IX"
+    roman[5] = "V"
+    roman[4] = "IV"
+    roman[1] = "I"
 
-        roman = OrderedDict()
-        roman[1000] = "M"
-        roman[900] = "CM"
-        roman[500] = "D"
-        roman[400] = "CD"
-        roman[100] = "C"
-        roman[90] = "XC"
-        roman[50] = "L"
-        roman[40] = "XL"
-        roman[10] = "X"
-        roman[9] = "IX"
-        roman[5] = "V"
-        roman[4] = "IV"
-        roman[1] = "I"
-    
-        def roman_num(num):
-            for r in roman.keys():
-                x, y = divmod(num, r)
-                yield roman[r] * x
-                num -= (r * x)
-                if num > 0:
-                    roman_num(num)
-                else:
-                    break
-    
-        return "".join([a for a in roman_num(num)])
+    def roman_num(num):
+        for r in roman.keys():
+            x, y = divmod(num, r)
+            yield roman[r] * x
+            num -= (r * x)
+            if num > 0:
+                roman_num(num)
+            else:
+                break
+
+    return "".join([a for a in roman_num(num)])
