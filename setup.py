dfbb3c57aae5f80c350035e8511a83f4648a7c72
shubham039 <32260408+shubham039@users.noreply.github.com>
2017-12-08 18:21:35 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,12 +10,12 @@ from setuptools import setup
 setup(
   name = 'GenerateRoman',
   packages = ['GenerateRoman'], # this must be the same as the name above
-  version = '0.1',
+  version = '0.3',
   description = 'Generate Roman no from an integer',
   author = 'Shubham Ringne',
   author_email = 'shubhamringne@gmail.com',
   url = 'https://github.com/shubham039/GetRoman', # use the URL to the github repo
-  download_url = 'https://github.com/shubham039/GetRoman/archive/0.1.tar.gz', # I'll explain this in a second
+  download_url = 'https://github.com/shubham039/GetRoman/archive/0.3.tar.gz', # I'll explain this in a second
   keywords = ['roman', 'convert', 'integer to roman'], # arbitrary keywords
   classifiers = [],
 )
